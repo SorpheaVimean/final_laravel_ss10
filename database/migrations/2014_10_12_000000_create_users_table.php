05891b39ec42bc08b5f5ffcc1b0e9dbc6d7a09f6
SorpheaVimean <sorphea.vomean@gmail.com>
2024-04-09 18:28:01 +0700

    init

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->integer('roleId');
+            $table->string('name');
+            $table->integer('gender');
+            $table->date('dob');
+            $table->integer('tel')->nullable();
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('address')->nullable();
+            $table->integer('status')->default(1);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
